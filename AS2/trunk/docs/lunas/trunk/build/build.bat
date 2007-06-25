4c93bd61a7ccdaf477514000ee25899d14a0fb0c
malcaraz <malcaraz@27c54971-031a-0410-9408-77ae697a8b7f>
2007-06-25 05:54:51 +0000

    AS2 VEGAS & extensions update (RC)

---
+++ AS2/trunk/docs/lunas/trunk/build/build.bat
@@ -0,0 +1,27 @@
+
+@echo off
+
+rem ############# Vegas Documentation properties
+
+
+set root=../../../..
+
+set as2api=%root%/tools/as2api/as2api.exe
+
+set output=../deploy/
+
+set src=%root%/src
+
+set macromedia=..\dependencies
+
+rem ############# Build the documentation
+
+set classpaths=vegas.* pegas.* asgard.* lunas.*
+
+set config=--classpath "%src%" --output-dir "%output%" --sources
+
+echo Build the documentation
+
+start %as2api% %classpaths% %config%
+
+pause
