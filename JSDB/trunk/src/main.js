5be7da965230ed99d09a7843bda6e6592c81ab08
malcaraz <malcaraz@27c54971-031a-0410-9408-77ae697a8b7f>
2008-03-06 16:08:41 +0000

    Little minor refactoring in JSDB project.

--- JSDB/trunk/src/main.js
+++ JSDB/trunk/src/main.js
@@ -17,5 +17,9 @@ var equal = hash == '93fc1e28bc17af6420552b746af10f4f' ;
  
 trace("'calista' MD5 result : " + hash + " : " + equal ) ;
 
+// E4X
+
+var x = <item><subitem>bonjour</subitem><subitem>hello</subitem><subitem>hi</subitem></item> ;
+trace( x.subitem[0] ) ;
 
 // TODO find and fix the __resolve methods in the framework.
