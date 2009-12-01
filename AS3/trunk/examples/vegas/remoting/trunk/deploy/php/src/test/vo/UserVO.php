c3a9120c0839e053eabb0859c1c4556de1563821
ekameleon <ekameleon@27c54971-031a-0410-9408-77ae697a8b7f>
2009-12-01 22:52:16 +0000

    Change licence with new date range : 2004-2010

--- AS3/trunk/examples/vegas/remoting/trunk/deploy/php/src/test/vo/UserVO.php
+++ AS3/trunk/examples/vegas/remoting/trunk/deploy/php/src/test/vo/UserVO.php
@@ -16,7 +16,7 @@
   
   The Initial Developer of the Original Code is
   ALCARAZ Marc (aka eKameleon)  <ekameleon@gmail.com>.
-  Portions created by the Initial Developer are Copyright (C) 2004-2009
+  Portions created by the Initial Developer are Copyright (C) 2004-2010
   the Initial Developer. All Rights Reserved.
   
   Contributor(s) :
--- AS3/trunk/unittests/src/vegas/core/AllTests.as
+++ AS3/trunk/unittests/src/vegas/core/AllTests.as
@@ -14,7 +14,7 @@
   
   The Initial Developer of the Original Code is
   ALCARAZ Marc (aka eKameleon)  <vegas@ekameleon.net>.
-  Portions created by the Initial Developer are Copyright (C) 2004-2008
+  Portions created by the Initial Developer are Copyright (C) 2004-2010
   the Initial Developer. All Rights Reserved.
   
   Contributor(s) :
